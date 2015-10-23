371383a35090d6a2e7cb917a87b1e4ab319cf2ad
Murat Cakmak <oguzhanmc@gmail.com>
2015-10-22 23:24:00 -0400

    It has all the functionality

--- MarkRetroCalculator/ViewController.swift
+++ MarkRetroCalculator/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var outLabel: UILabel!
     
+    @IBAction func clearEverything(sender: AnyObject) {
+        leftOperand = ""
+        rightOperand = ""
+        result = ""
+        operation = 0
+        currentOperand = ""
+        outLabel.text = ""
+
+        
+    }
     var btnSound: AVAudioPlayer!
     
     var leftOperand: String = ""
@@ -44,29 +54,33 @@ class ViewController: UIViewController {
     @IBAction func numberPressed(btn: UIButton!){
         soundPlay()
         if btn.tag > 9 {
-            if operation != 0 {
+            if operation != 0  && rightOperand == "" && leftOperand != "" {
                 rightOperand = currentOperand
                 if operation == 10 {
                     result = String(Double(leftOperand)! / Double(rightOperand)!)
-                    currentOperand = result
-                    outLabel.text = currentOperand
-                    operation = btn.tag
+                    updateValues(btn.tag)
                 }
                 else if operation == 11 {
-                    
+                    result = String(Double(leftOperand)! * Double(rightOperand)!)
+                    updateValues(btn.tag)
                 }
                 else if operation == 12 {
-                    
+                    result = String(Double(leftOperand)! - Double(rightOperand)!)
+                    updateValues(btn.tag)
                 }
                 else if operation == 13 {
-                    
+                    result = String(Double(leftOperand)! + Double(rightOperand)!)
+                    updateValues(btn.tag)
                 }
                 else if operation == 14 {
-                    
+                    leftOperand = currentOperand
+                    rightOperand = ""
+                    operation = 0
                 }
-
+                
                 
             }
+                
             else{
                 if btn.tag > 9 {
                     operation = btn.tag
@@ -93,6 +107,14 @@ class ViewController: UIViewController {
         }
             btnSound.play()
         
+    
+    }
+    func updateValues(tag: Int){
+        currentOperand = result
+        outLabel.text = currentOperand
+        rightOperand = ""
+        leftOperand = currentOperand
+        operation = tag
     
     }
 
